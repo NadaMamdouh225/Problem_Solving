164d7b5c4702a7876382579555b26a678f5525ae
NadaMamdouh225 <105312001+NadaMamdouh225@users.noreply.github.com>
2023-09-19 12:02:13 +0300

    modify code

--- HackerEarth/PairSum/PairSum.c
+++ HackerEarth/PairSum/PairSum.c
@@ -1,25 +1,37 @@
 #include <stdio.h>
+#include <stdbool.h>
 typedef struct
 {
 	int num1;
 	int num2;
 }Result;
 
-int isPrime(int num);
+#define N_Size 1000000
+bool Prime[N_Size+1];
+void isPrime(void);
 Result pairSum (int num);
 
 int main(){
+	int t;
+	isPrime();
+	scanf("%d",&t);
+	while(t--)
+	{
 	int num;
 	Result saveVal;
-	scanf("%d", &num);              	                  // Reading input from STDIN
+	scanf("%d", &num);              	                 
 	saveVal = pairSum(num);
-	if(saveVal.num1 != -1 && saveVal.num2 != -1 && num > 2)
+	if(saveVal.num1 != -1 && saveVal.num2 != -1 )
 	{
 		printf("%d %d",saveVal.num1,saveVal.num2);
+		
 	}
 	else
 	{
 		printf("%d",saveVal.num1);
+		
+	}
+	printf("\n");
 	}
 }
 Result pairSum (int num)
@@ -27,33 +39,33 @@ Result pairSum (int num)
 	Result sol;
 	sol.num1 = -1;
 	sol.num2 = -1;
-	for(int i =2;i<num;i++)
+	for(int i =2;i<=num;i++)
 	{
-		for(int j =2;j<num;j++)
-		{
-			if( isPrime(i) && isPrime(j) )
-			{
-				if(i + j == num)
-				{    
-					sol.num1 = i ;
-	                sol.num2 = j ;
-				    return sol;	
-				}
-			}
+		if((i + num-i == num) && Prime[i] && Prime[num-i] )
+		{  
+			sol.num1 = i ;
+	        sol.num2 = num-i ;
+			return sol;	
 		}
-		
 	}
 	return sol;
 }
 
-int isPrime(int num)
+void isPrime(void)
 {
-	for(int i=2;i<num;i++)
+	memset(Prime,true,sizeof(Prime));
+	Prime[0] = Prime[1] = false;
+	// Sieve of Eratosthenes to calculate prime numbers
+	for(int i =2;i*i<=N_Size;i++)
 	{
-		if(num % i == 0)
+		// if unmarked then it's a prime
+		if(Prime[i])
 		{
-			return 0;      // not prime number
+			// then mark all multiples of Prime[i]
+		    for(int j = i*i; j <= N_Size; j+=i)
+		   {
+		    	Prime[j] = false;
+	     	}
 		}
 	}
-	return 1;  //  prime number
 }
