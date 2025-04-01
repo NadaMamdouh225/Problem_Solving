e35f79e71d5bc734de833eb697d9fc04b3ecc471
Nada Mamdouh <105312001+NadaMamdouh225@users.noreply.github.com>
2025-04-02 00:13:05 +0200

    983. Minimum Cost For Tickets

---
+++ LeetCode/Minimum_Cost_For_Tickets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int dp[366];
+    int solve(int i,vector<int>& days, vector<int>& costs)
+    {
+        //base case
+        if(i == days.size()) return 0;
+        int &ret = dp[i];
+        if(~ret) return ret;           // if ret value was solved before then return
+        ret = 1e9;
+        ret = min(ret, solve(i+1,days,costs) + costs[0]);
+        int idx = lower_bound(days.begin(),days.end(),days[i]+7) - days.begin();
+        ret = min(ret, solve(idx,days,costs) + costs[1]);
+        idx = lower_bound(days.begin(),days.end(),days[i]+30) - days.begin();
+        ret = min(ret, solve(idx,days,costs) + costs[2]);
+        return ret;
+    }
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        memset(dp,-1, sizeof dp);
+        return solve(0, days, costs);
+    }
+};
